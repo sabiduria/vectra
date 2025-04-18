5b72567e40757c2e27a6d7fe9d16534ae7c37c02
sabiduria <antdepadoue@gmail.com>
2025-04-18 15:41:40 +0200

    Expenses Management

--- templates/Expenses/add.php
+++ templates/Expenses/add.php
@@ -5,23 +5,23 @@
  * @var \Cake\Collection\CollectionInterface|string[] $shops
  * @var \Cake\Collection\CollectionInterface|string[] $expensestypes
  */
-$this->set('title_2', 'Expenses');
+$this->set('title_2', 'Depenses');
 $emptyText = "Veuillez selectionner";
 ?>
 <div class="mt-3">
     <?= $this->Form->create($expense) ?>
         <div class="row gy-2">
             <div class="col-xl-12">
-                <?= $this->Form->control('shop_id', ['options' => $shops, 'empty' => $emptyText, 'class' => 'form-select js-example-basic-single', 'label' => 'shop_id']); ?>
+                <?= $this->Form->control('shop_id', ['options' => $shops, 'empty' => $emptyText, 'class' => 'form-select js-example-basic-single', 'label' => 'Shop']); ?>
             </div>
             <div class="col-xl-12">
-                <?= $this->Form->control('expensestype_id', ['options' => $expensestypes, 'empty' => $emptyText, 'class' => 'form-select js-example-basic-single', 'label' => 'expensestype_id']); ?>
+                <?= $this->Form->control('expensestype_id', ['options' => $expensestypes, 'empty' => $emptyText, 'class' => 'form-select js-example-basic-single', 'label' => 'Type de depense']); ?>
             </div>
             <div class="col-xl-12">
-                <?= $this->Form->control('amount', ['class' => 'form-control', 'label' => 'amount']); ?>
+                <?= $this->Form->control('amount', ['class' => 'form-control', 'label' => 'Montant']); ?>
             </div>
             <div class="col-xl-12">
-                <?= $this->Form->control('description', ['class' => 'form-control', 'label' => 'description']); ?>
+                <?= $this->Form->control('description', ['class' => 'form-control', 'label' => 'Description']); ?>
             </div>
         </div>
         <div class="mt-3 mb-3">
--- templates/Expenses/index.php
+++ templates/Expenses/index.php
@@ -3,46 +3,38 @@
  * @var \App\View\AppView $this
  * @var iterable<\App\Model\Entity\Expense> $expenses
  */
-$this->set('title_2', 'Expenses');
+$this->set('title_2', 'Depenses');
 $Number = 1;
 ?>
 <div class="mt-3">
-    <button class="btn btn-sm btn-primary-light mb-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#NewItem" aria-controls="NewItem"><i class="fa-thin fa-plus"></i> Ajouter</button>
-    <?= $this->Html->link(__('Nouveau Expense'), ['action' => 'add'], ['class' => 'btn btn-success btn-sm mb-3']) ?>
+    <?= $this->Html->link(__('Nouvelle dépense'), ['action' => 'add'], ['class' => 'btn btn-success btn-sm mb-3']) ?>
     <div class="table-responsive">
         <table id="scroll-vertical" class="table table-bordered text-nowrap w-100 TableData">
             <thead>
                 <tr>
                     <th><?= $this->Paginator->sort('N°') ?></th>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('shop_id') ?></th>
-                    <th><?= $this->Paginator->sort('expensestype_id') ?></th>
-                    <th><?= $this->Paginator->sort('amount') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th><?= $this->Paginator->sort('createdby') ?></th>
-                    <th><?= $this->Paginator->sort('modifiedby') ?></th>
-                    <th><?= $this->Paginator->sort('deleted') ?></th>
+                    <th><?= $this->Paginator->sort('Shop') ?></th>
+                    <th><?= $this->Paginator->sort('Type') ?></th>
+                    <th><?= $this->Paginator->sort('Description') ?></th>
+                    <th><?= $this->Paginator->sort('Montant') ?></th>
+                    <th><?= $this->Paginator->sort('Date') ?></th>
+                    <th><?= $this->Paginator->sort('Par') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($expenses as $expense): ?>
+                <?php foreach ($expenses as $expense) : ?>
                 <tr>
                     <td><?= $Number++ ?></td>
-                    <td><?= $this->Number->format($expense->id) ?></td>
-                    <td><?= $expense->hasValue('shop') ? $this->Html->link($expense->shop->name, ['controller' => 'Shops', 'action' => 'view', $expense->shop->id]) : '' ?></td>
-                    <td><?= $expense->hasValue('expensestype') ? $this->Html->link($expense->expensestype->name, ['controller' => 'Expensestypes', 'action' => 'view', $expense->expensestype->id]) : '' ?></td>
-                    <td><?= $expense->amount === null ? '' : $this->Number->format($expense->amount) ?></td>
+                    <td><?= $expense->shop->name ?></td>
+                    <td><?= $expense->expensestype->name ?></td>
+                    <td><?= $expense->description ?></td>
+                    <td><?= $this->Number->format($expense->amount) ?></td>
                     <td><?= h($expense->created) ?></td>
-                    <td><?= h($expense->modified) ?></td>
                     <td><?= h($expense->createdby) ?></td>
-                    <td><?= h($expense->modifiedby) ?></td>
-                    <td><?= h($expense->deleted) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('Details'), ['action' => 'view', $expense->id], ['class' => 'btn btn-success btn-sm']) ?>
-                        <?= $this->Html->link(__('Editer'), ['action' => 'edit', $expense->id], ['class' => 'btn btn-primary btn-sm']) ?>
-                        <?= $this->Form->postLink(__('Supprimer'), ['action' => 'delete', $expense->id], ['class' => 'btn btn-danger btn-sm', 'confirm' => __('Voulez-vous supprimer cette information ?')]) ?>
+                    <td class="text-end">
+                        <?= $this->Html->link(__('<i class="ri-pencil-line"></i>'), ['action' => 'edit', $expense->id], ['class' => 'btn btn-primary btn-sm', 'escape' => false]) ?>
+                        <?= $this->Form->postLink(__('<i class="ri-delete-bin-line"></i>'), ['action' => 'delete', $expense->id], ['class' => 'btn btn-danger btn-sm', 'confirm' => __('Voulez-vous supprimer cette information ?'), 'escape' => false]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
@@ -50,71 +42,3 @@ $Number = 1;
         </table>
     </div>
 </div>
-
-<div class="offcanvas offcanvas-end" tabindex="-1" id="NewItem"
-     aria-labelledby="offcanvasRightLabel1">
-    <div class="offcanvas-header border-bottom border-block-end-dashed">
-        <h5 class="offcanvas-title" id="offcanvasRightLabel1">Nouveau Expenses</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body p-3">
-        <div class="row">
-            <div id="response"></div>
-<div class="mt-3">
-    <?= $this->Form->create(null, ['id' => 'DataForm']);?>
-        <div class="row gy-2">
-            <div class="col-xl-12">
-                <?= $this->Form->control('shop_id', ['options' => $shops, 'empty' => $emptyText, 'class' => 'form-select js-example-basic-single', 'label' => 'shop_id']); ?>
-            </div>
-            <div class="col-xl-12">
-                <?= $this->Form->control('expensestype_id', ['options' => $expensestypes, 'empty' => $emptyText, 'class' => 'form-select js-example-basic-single', 'label' => 'expensestype_id']); ?>
-            </div>
-            <div class="col-xl-12">
-                <?= $this->Form->control('amount', ['class' => 'form-control', 'label' => 'amount']); ?>
-            </div>
-            <div class="col-xl-12">
-                <?= $this->Form->control('description', ['class' => 'form-control', 'label' => 'description']); ?>
-            </div>
-        </div>
-        <div class="mt-3 mb-3">
-            <?= $this->Form->button(__('Enregistrer'), ['class'=>'btn btn-success']) ?>
-        </div>
-    <?= $this->Form->end() ?>
-</div>
-
-        </div>
-    </div>
-</div>
-
-<script>
-    $(document).ready(function() {
-        $('#DataForm').on('submit', function(e) {
-            e.preventDefault(); // Prevent default form submission
-            // Get form data
-            var formData = $(this).serialize();
-
-            // Perform AJAX request
-            $.ajax({
-                url: '<?= $this->Url->build(["controller" => 'Expenses', 'action' => 'insert']) ?>',
-                method: 'POST',
-                data: formData,
-                dataType: 'json', // Expecting JSON in the response
-                success: function(response) {
-                    console.log(response.data); // Log the JSON response
-                    $('#response').html('<div class="alert alert-success rounded-pill alert-dismissible fade show mb-1 mt-2">' + response.message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><i class="bi bi-x"></i></button> </div>'); // Show success message
-                    var newRow = '<tr>';
-                    newRow += '<td>'+''+'</td>'; // Add your actions
-                    newRow += '</tr>';
-
-                    // Append the new row to the table
-                    $('.TableData tbody').append(newRow);
-                    $('#DataForm')[0].reset();
-                },
-                error: function(xhr, status, error) {
-                    console.error(error); // Log any error
-                    $('#response').html('<p>An error occurred. Please try again.</p>');
-                }
-            });
-        });
-    });
-</script>
